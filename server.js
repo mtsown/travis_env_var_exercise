c0777be0ec9141b86e63dd322aa84bb2e6fbd46a
Son Nguyen <snguyenvie@gmail.com>
2022-09-02 09:19:21 +0700

    add env variables

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+const FAVORTITE_INSTRUCTOR = process.env.NAME;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log("Microservices rock!");
+  while (true) {
+    console.log(`${FAVORTITE_INSTRUCTOR} rocks!`);
     await sleep(5000);
   }
 }
